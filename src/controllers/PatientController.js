a55d00248fe646eb648542feef1b7550d15cec94
alaa-assas <alaa.assas1997@gmail.com>
2025-12-27 19:55:28 +0300

    add structure of project

---
+++ src/controllers/PatientController.js
@@ -0,0 +1,43 @@
+class PatientController {
+    async getAll(req, res) {
+
+        // TODO: Implement get all logic
+        res.json({ success: true, data: [] });
+    
+    }
+
+    async getById(req, res) {
+        
+        const { id } = req.params;
+        // TODO: Implement get by ID logic
+        res.json({ success: true, data: { id } });
+        
+    }
+
+    async create(req, res) {
+        
+        const data = req.body;
+
+        // TODO: Implement create logic
+        res.status(201).json({ success: true, data });
+        
+    }
+
+    async update(req, res) {
+
+        const { id } = req.params;
+        const data = req.body;
+        // TODO: Implement update logic
+        res.json({ success: true, data: { id, ...data } });
+    }
+
+    async delete(req, res) {
+        
+        const { id } = req.params;
+        // TODO: Implement delete logic
+        res.json({ success: true, message: 'Deleted successfully' });
+        
+    }
+}
+
+module.exports = PatientController;
